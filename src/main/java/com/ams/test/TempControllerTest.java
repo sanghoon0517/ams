73c25feaeea14c6174bce341c316654e2422ab9a
dldudgjs31 <dldudgjs31@naver.com>
2022-07-10 01:14:22 +0900

    jsp -> 타임리프 템플릿 변경작업, layout 설정 추가 설정 진행중

--- src/main/java/com/ams/test/TempControllerTest.java
+++ src/main/java/com/ams/test/TempControllerTest.java
@@ -3,15 +3,29 @@ package com.ams.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class TempControllerTest {
 	
-	@GetMapping("/temp/jsp")
-	public String tempHome() {
-		return "template";
+	@GetMapping("/")
+	public String mainPage() {
+		return "common/main";
 	}
-	@GetMapping("/calendar")
-	public String temp1Home() {
-		return "calendar";
+	@GetMapping("/test")
+	public String temp() {
+		return "test";
+	}
+	@GetMapping("/studentEnroll")
+	public String enroll(){
+		return "enroll/student_enroll";
+	}
+	@GetMapping("/studentList")
+	public String enrollList(){
+		return "enroll/student_list";
+	}
+	@GetMapping("/schedule")
+	public String schedule(){
+		return "schedule/schedule";
+
 	}
 }
