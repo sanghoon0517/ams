a88a9692fa30c07c7fb25a442c1a1984a39d153f
jsh <jsh940517@daum.net>
2022-07-25 17:53:56 +0900

    pom.xml에 AOP 설정 추가 및 Exception 처리 설정 추가
    Signed-off-by: jsh <jsh940517@daum.net>

--- src/main/java/com/ams/handler/ex/AMSApiException.java
+++ src/main/java/com/ams/handler/ex/AMSApiException.java
@@ -2,12 +2,13 @@ package com.ams.handler.ex;
 
 import java.util.Map;
 
-public class CustomException extends RuntimeException{
+public class AMSApiException extends RuntimeException{
 
 	//객체를 구분할 때 쓰는 것
 	private static final long serialVersionUID = 1L;
 	
-	public CustomException(String message) {
+	public AMSApiException(String message) {
 		super(message); //부모한테 던져주기만 하면 됌
 	}
+	
 }
--- src/main/java/com/ams/handler/ex/AMSValidationApiException.java
+++ src/main/java/com/ams/handler/ex/AMSValidationApiException.java
@@ -2,18 +2,18 @@ package com.ams.handler.ex;
 
 import java.util.Map;
 
-public class CustomValidationApiException extends RuntimeException{
+public class AMSValidationApiException extends RuntimeException{
 
 	//객체를 구분할 때 쓰는 것
 	private static final long serialVersionUID = 1L;
 	
 	private Map<String, String> errorMap;
 	
-	public CustomValidationApiException(String message) {
+	public AMSValidationApiException(String message) {
 		super(message); //부모한테 던져주기만 하면 됌
 	}
 	
-	public CustomValidationApiException(String message, Map<String, String> errorMap) {
+	public AMSValidationApiException(String message, Map<String, String> errorMap) {
 		super(message); //부모한테 던져주기만 하면 됌
 		this.errorMap = errorMap;
 	}
