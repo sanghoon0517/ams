36f1f7382b2507c0ee32447797bc5d6e2235f642
dldudgjs31 <dldudgjs31@naver.com>
2022-07-26 23:08:23 +0900

    클래스 등록 1차 기능 완료, validation 작업 이후 진행 예정

--- src/main/java/com/ams/classes/controller/api/ClassesApiController.java
+++ src/main/java/com/ams/classes/controller/api/ClassesApiController.java
@@ -1,6 +1,8 @@
 package com.ams.classes.controller.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ams.classes.model.dto.ClassDto;
+import com.ams.classes.service.ClassService;
 import com.ams.common.model.dto.ResponseDto;
 import com.ams.teacher.model.dto.TeacherDto;
 import com.ams.teacher.service.TeacherSerivce;
@@ -21,20 +25,42 @@ public class ClassesApiController {
     @Autowired
     private TeacherSerivce service;
 
+    @Autowired
+    private ClassService classService;
     /**
-     * 선생님 리스트 출력
-     * @param dto
+     * 클래스 등록
+     * @param 
      * @return
      */
-    @PostMapping("/classes/teacherlist")
-	public ResponseEntity<?> idCheck(Model model) {
+    @PostMapping("/class")
+	public ResponseEntity<?> idCheck(@RequestBody ClassDto dto) {
         int code=1;
 		String msg= "";
 		String data = "";
-        //선생님 리스트 
-        List<TeacherDto> list =service.listDao();
-        model.addAttribute("list", list);
-		return new ResponseEntity<>(new ResponseDto<String>(code,msg,data), HttpStatus.OK);
+
+        //클래스 먼저 생성
+        int result = classService.insertClass(dto);
+        int result2 = 1;
+        if(result==0){
+            return new ResponseEntity<>(new ResponseDto<String>(code,msg,data), HttpStatus.BAD_REQUEST);
+        }else{
+            // 클래스 생성 성공이후 학생 테이블에 클래스 등록처리
+            HashMap<String,String> map = new HashMap<>();
+            map.put("c_nm", dto.getC_nm());
+            map.put("c_color",dto.getC_color());
+            map.put("c_detail",dto.getC_detail());
+            map.put("st_idx", dto.getSt_idx().get(0).toString());
+            for(int st_idx : dto.getSt_idx()){
+                map.replace("st_idx", Integer.toString(st_idx));
+                result2 = classService.updateStClass(map);
+            }
+            if(result2==0){
+                return new ResponseEntity<>(new ResponseDto<String>(code,msg,data), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(new ResponseDto<String>(code,msg,data), HttpStatus.OK);
+        }
+
+
 
 	}
 }
